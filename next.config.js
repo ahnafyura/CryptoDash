c7bddb7dc60749e654fe0b085d2ab7d5de2fa10b
Ahnaf Al Ghiffarri Ahtasyafi <email-ahnafalghiffarriahtasyafi@gmail.com>
2026-02-11 14:00:07 +0700

    untuk deploy

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ['assets.coingecko.com'],
   },
-}
+  // --- TAMBAHKAN BAGIAN INI ---
+  eslint: {
+    // Warning: Ini membolehkan build sukses meski ada error linter
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: Ini membolehkan build sukses meski ada error typescript
+    ignoreBuildErrors: true,
+  },
+  // ---------------------------
+};
 
-module.exports = nextConfig
+export default nextConfig;
